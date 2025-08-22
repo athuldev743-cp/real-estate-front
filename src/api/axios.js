f7eaebb62d69bf56d542041018f33079e0831279
athul dev <athuldev743@gmail.com>
2025-08-22 11:13:18 +0530

    Update axios baseURL and remove unused files

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://back-end-lybr.onrender.com",
 });
 
 // Attach token automatically if present
