32befce32442c25b903c32439c700c5b0530fb7a
athul dev <athuldev743@gmail.com>
2025-08-22 08:39:43 +0530

    Fix BrowserRouter basename and image paths for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-import ReactDOM from "react-dom/client";
 import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <HashRouter>
+ReactDOM.render(
+  <BrowserRouter basename="/real-estate-frontend">
     <App />
-  </HashRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
