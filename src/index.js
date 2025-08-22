0d5c8ec0552bdddb51489c5679e9338195e89262
athul dev <athuldev743@gmail.com>
2025-08-22 09:02:46 +0530

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+// index.js
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
-  <BrowserRouter basename="/real-estate-frontend">
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </HashRouter>
 );
