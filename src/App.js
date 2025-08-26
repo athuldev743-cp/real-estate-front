493bfa1eafc6ae98fdf8d7dcaa4f9d2c8cbe0995
athul dev <athuldev743@gmail.com>
2025-08-27 03:47:39 +0530

    changera

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import AddProperty from "./pages/AddProperty";
+import Search from "./pages/Search";
 import Category from "./pages/Category";
+import AddProperty from "./pages/AddProperty";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Search from "./pages/Search";
+import PropertyDetails from "./pages/PropertyDetails";
 
-function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/add-property" element={<AddProperty />} />
-      <Route path="/category/:category" element={<Category />} />
-      <Route path="/top-deals" element={<Category />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/search" element={<Search />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/category/:category" element={<Category />} />
+        <Route path="/top-deals" element={<Category />} /> {/* Top Deals fetches all */}
+        <Route path="/add-property" element={<AddProperty />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
