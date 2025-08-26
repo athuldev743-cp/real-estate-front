d4d77000d0df36d93e8d2fa70c21c6b7b32eef9e
athul dev <athuldev743@gmail.com>
2025-08-27 05:08:34 +0530

    3333335

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Category from "./pages/Category";
@@ -10,18 +10,16 @@ import PropertyDetails from "./pages/PropertyDetails";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/category/:category" element={<Category />} />
-        <Route path="/top-deals" element={<Category />} /> {/* Top Deals fetches all */}
-        <Route path="/add-property" element={<AddProperty />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/property/:id" element={<PropertyDetails />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/category/:category" element={<Category />} />
+      <Route path="/top-deals" element={<Category />} />
+      <Route path="/add-property" element={<AddProperty />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/property/:id" element={<PropertyDetails />} />
+    </Routes>
   );
 }
 
