ef5598cd1cdc011ccddcfa01c344fcc71c0369b2
athul dev <athuldev743@gmail.com>
2025-08-28 06:55:03 +0530

    333390

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-// App.js
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Account from "./pages/Account";
+import AddProperty from "./pages/AddProperty";
+import Category from "./pages/Category";
+import PropertyDetails from "./pages/PropertyDetails";
+import Search from "./pages/Search";
+
+// API
 import { getCurrentUser } from "./api/PropertyAPI";
 
 function App() {
@@ -27,14 +34,13 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home user={user} />} />
-      <Route
-        path="/register"
-        element={user ? <Navigate to="/" /> : <Register />}
-      />
-      <Route
-        path="/account"
-        element={user ? <Account user={user} /> : <Navigate to="/register" />}
-      />
+      <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+      <Route path="/account" element={user ? <Account user={user} /> : <Navigate to="/register" />} />
+      <Route path="/add-property" element={user ? <AddProperty user={user} /> : <Navigate to="/register" />} />
+      <Route path="/category/:category" element={<Category />} />
+      <Route path="/property/:id" element={<PropertyDetails userId={user?._id} />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
